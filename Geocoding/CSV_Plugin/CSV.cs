2fc67e0e280649e048347a08b5b10a7e7ace7831
felixkremser <82540@fhwn.ac.at>
2015-01-12 19:22:29 +0100

    felix ilja änderungen

---
+++ Geocoding/CSV_Plugin/CSV.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data;
+
+namespace CSV_Plugin
+{
+    public static class CSV
+    {
+        public static int get100()
+        {
+            return 100;
+        }
+
+        public static DataTable importCSV(string path)
+        {
+            //initialising a StreamReader type variable and will pass the file location
+
+            StreamReader oStreamReader = new StreamReader(path);
+
+            DataTable oDataTable = null;
+            int RowCount = 0;
+            string[] ColumnNames = null;
+            string[] oStreamDataValues = null;
+            //using while loop read the stream data till end
+            while (!oStreamReader.EndOfStream)
+            {
+                String oStreamRowData = oStreamReader.ReadLine().Trim();
+                if (oStreamRowData.Length > 0)
+                {
+                    oStreamDataValues = oStreamRowData.Split(';');
+                    //Bcoz the first row contains column names, we will poluate 
+                    //the column name by
+                    //reading the first row and RowCount-0 will be true only once
+                    if (RowCount == 0)
+                    {
+                        RowCount = 1;
+                        ColumnNames = oStreamRowData.Split(';');
+                        oDataTable = new DataTable();
+
+                        //using foreach looping through all the column names
+                        foreach (string csvcolumn in ColumnNames)
+                        {
+                            DataColumn oDataColumn = new DataColumn(csvcolumn.ToUpper(), typeof(string));
+
+                            //setting the default value of empty.string to newly created column
+                            oDataColumn.DefaultValue = string.Empty;
+
+                            //adding the newly created column to the table
+                            oDataTable.Columns.Add(oDataColumn);
+                        }
+                    }
+                    else
+                    {
+                        //creates a new DataRow with the same schema as of the oDataTable            
+                        DataRow oDataRow = oDataTable.NewRow();
+
+                        //using foreach looping through all the column names
+                        for (int i = 0; i < ColumnNames.Length; i++)
+                        {
+                            oDataRow[ColumnNames[i]] = oStreamDataValues[i] == null ? string.Empty : oStreamDataValues[i].ToString();
+                        }
+
+                        //adding the newly created row with data to the oDataTable       
+                        oDataTable.Rows.Add(oDataRow);
+                    }
+                }
+            }
+
+
+            //close the oStreamReader object
+            oStreamReader.Close();
+            //release all the resources used by the oStreamReader object
+            oStreamReader.Dispose();
+
+            return oDataTable;
+
+
+            ////Looping through all the rows in the Datatable
+            //foreach (DataRow oDataRow in oDataTable.Rows)
+            //{
+
+            //    string RowValues = string.Empty;
+
+            //    //Looping through all the columns in a row
+
+            //    foreach (string csvcolumn in ColumnNames)
+            //    {
+            //        //concatenating the values for display purpose
+            //        RowValues += csvcolumn + "=" + oDataRow[csvcolumn].ToString() + ";  ";
+            //    }
+            //    //Displaying the result on the console window
+            //    Console.WriteLine(RowValues);
+            //}
+        }
+    }
+}
--- Geocoding/Geocoding/MainWindow.xaml.cs
+++ Geocoding/Geocoding/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,51 @@ namespace Geocoding
     /// </summary>
     public partial class MainWindow : Window
     {
+        DataTable myDataTable;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //FELIX TEST "CSV_Plugin"
+
+            string currentDir = Environment.CurrentDirectory;
+            DirectoryInfo directory = new DirectoryInfo(currentDir);
+            string fullDirectory = directory.FullName;
+            MessageBox.Show(fullDirectory);
+
+            //string fullFile = file.FullName;
+
+            //MessageBox.Show(CSV_Plugin.CSV.get100().ToString()); //nur eine methode zum testen die 100 als int wert liefert
+            //String testpath2 = "C:\\Temp\\GitHub\\geocoding-project\\Geocoding\\CSV_Plugin\\test.csv";
+            String testpath = @"C:\Temp\GitHub\geocoding-project\Geocoding\CSV_Plugin\test.csv";
+                        
+            if (File.Exists(testpath))
+            {
+                myDataTable = CSV_Plugin.CSV.importCSV(testpath);
+            }
+            else
+            {
+                MessageBox.Show("File does not exist: " + testpath);
+            }
+            
+            
+            //ILJA
+            /*DataTable myDataTable = new DataTable();
+            var comboColumn = new DataGridComboBoxColumn();
+            comboColumn.Header = new ComboBox();
+
+            myDataTable.Columns.Add("Column A");
+            myDataTable.Columns.Add("Column B");
+
+            // Add some rows to the DataTable.
+            myDataTable.Rows.Add("A1", "B1");
+            myDataTable.Rows.Add("A2", "B2");
+            myDataTable.Rows.Add("A3", "B3");*/
+
+            // Bind DataTable to DataGrid.
+            grid1.ItemsSource = myDataTable.DefaultView;
+            //grid1.Columns[0].Header = comboColumn;
         }
     }
 }
