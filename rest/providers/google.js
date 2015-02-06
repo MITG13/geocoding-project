f03b771033875d119d3e6349f022bd79dc5fe002
Thomas Rupprecht <rupprecht.thomas+git@gmail.com>
2015-02-06 16:05:04 +0100

    extract polyfill

--- rest/providers/google.js
+++ rest/providers/google.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('../polyfill.js')();
+
 var request = require('request');
 
 exports = module.exports = {};
@@ -85,30 +87,5 @@ function parseJSON(body) {
             "coordinates": [result.geometry.location.lat, result.geometry.location.lng]
         },
         "epsg": "EPSG:4326"
-    };
-}
-
-
-// Polyfill
-if (!Array.prototype.find) {
-    Array.prototype.find = function(predicate) {
-        if (this === null) {
-            throw new TypeError('Array.prototype.find called on null or undefined');
-        }
-        if (typeof predicate !== 'function') {
-            throw new TypeError('predicate must be a function');
-        }
-        var list = Object(this);
-        var length = list.length >>> 0;
-        var thisArg = arguments[1];
-        var value;
-
-        for (var i = 0; i < length; i++) {
-            value = list[i];
-            if (predicate.call(thisArg, value, i, list)) {
-                return value;
-            }
-        }
-        return undefined;
     };
 }
